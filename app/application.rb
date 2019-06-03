853a6ee48b5f11d6f3c190d33ec1dde1f3a0b810
rudi hinds <rudihinds@gmail.com>
2019-06-03 08:27:53 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -23,7 +23,7 @@ class Application
           @@cart.each {|item| resp.write "#{item}\n"}
         end
     elsif req.path.match(/add/)
-      item_to_add = req.params("item")
+      item_to_add = req.params["item"]
       if @@items.include? item_to_add
          @@cart << item_to_add
          resp.write "added #{item_to_add}"
